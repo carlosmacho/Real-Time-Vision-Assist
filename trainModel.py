a625b72daa06b9a91b0a9e370ffae43c878601a0
Carlos Macho <carlosmacho@ipvc.pt>
2024-06-16 23:29:31 +0100

    Code cleaning

--- trainModel.py
+++ trainModel.py
@@ -43,24 +43,6 @@ def check_gpu():
         print_failure("nvidia-smi command not found. Ensure that the NVIDIA drivers are installed and the PATH is set correctly.")
         return False
 
-def install_packages():
-    try:
-        # Install the supervision package
-        subprocess.run(['pip', 'install', '-q', 'supervision'], check=True)
-
-        # Install the yolov10 package from the GitHub repository
-        subprocess.run(['pip', 'install', '-q', 'git+https://github.com/THU-MIG/yolov10.git'], check=True)
-
-        # Install the roboflow package
-        subprocess.run(['pip', 'install', 'roboflow'], check=True)
-
-        print_success("Packages installed successfully.")
-        return True
-
-    except subprocess.CalledProcessError:
-        print_failure("Error installing packages.")
-        return False
-
 def download_weights():
     try:
         # Get the current working directory
@@ -99,7 +81,7 @@ def download_dataset():
     try:
         # Initialize Roboflow with the API key
         ROBOFLOW_API_KEY = os.getenv('ROBOFLOW_API_KEY')
-        rf = Roboflow(api_key="JaM7HVDRmrbbCNrYVam0")
+        rf = Roboflow(api_key="ROBOFLOW_API_KEY")
 
         # Access the specific project and version
         project = rf.workspace("my-workspace-zdfxw").project("yolo-real-time-object-detection")
@@ -128,8 +110,8 @@ def train_yolo():
         os.chdir(HOME)
         
         # Define the path to the YOLO model and dataset location
-        yolo_model_path = os.path.join(HOME, "weights", "yolov8n.pt")
-        dataset_location = "C:\\Users\\Carlos\\vsProjects\\CP3\\Yolo-Real-Time-Object-Detection-4" # Update this with the actual dataset location
+        yolo_model_path = os.path.join(HOME, "weights", "yolov8m.pt")
+        dataset_location = "C:\\Users\\Carlos\\vsProjects\\AOOP-CP3\\Yolo-Real-Time-Object-Detection-4" # Update this with the actual dataset location
 
         # Run the YOLO training command
         command = [
@@ -152,79 +134,13 @@ def train_yolo():
     except subprocess.CalledProcessError:
         print_failure("Error during YOLO training.")
         return False
-
-def list_directory():
-    try:
-        # Get the current working directory
-        HOME = os.getcwd()
-
-        # Define the path to the directory you want to list
-        directory_path = os.path.join(HOME, "runs", "detect", "train10")
-
-        # List the contents of the directory
-        if os.path.exists(directory_path):
-            for item in os.listdir(directory_path):
-                print(item)
-            print_success("Directory listed successfully.")
-            return True
-        else:
-            print_failure(f"The directory {directory_path} does not exist.")
-            return False
-
-    except Exception as e:
-        print_failure(f"Error listing directory: {e}")
-        return False
-
-def display_confusion_matrix():
-    try:
-        # Get the current working directory
-        HOME = os.getcwd()
-
-        # Change to the HOME directory
-        os.chdir(HOME)
-        
-        # Define the path to the confusion matrix image
-        image_path = os.path.join(HOME, "runs", "detect", "train10", "confusion_matrix.png")
-        
-        # Display the image
-        display(Image(filename=image_path, width=600))
-        print_success("Confusion matrix displayed successfully.")
-        return True
-
-    except Exception as e:
-        print_failure(f"Error displaying confusion matrix: {e}")
-        return False
-
-def display_results_image():
-    try:
-        # Get the current working directory
-        HOME = os.getcwd()
-
-        # Change to the HOME directory
-        os.chdir(HOME)
-        
-        # Define the path to the results image
-        image_path = os.path.join(HOME, "runs", "detect","train10", "results.png")
-        
-        # Display the image
-        display(Image(filename=image_path, width=600))
-        print_success("Results image displayed successfully.")
-        return True
-   
-    except Exception as e:
-        print_failure(f"Error displaying results image: {e}")
-        return False
-
+    
 def main():
     steps = {
         1: check_gpu,
-        2: install_packages,
-        3: download_weights,
-        4: download_dataset,
-        5: train_yolo,
-        6: list_directory,
-        7: display_confusion_matrix,
-        8: display_results_image
+        2: download_weights,
+        3: download_dataset,
+        4: train_yolo,
     }
 
     while True:
@@ -233,16 +149,11 @@ def main():
         print("==========================================\n")
         print("Choose a step to run (1-8), or '0' to exit:\n")
         print("1. Check GPU")
-        print("2. Install packages")
-        print("3. Download weights")
-        print("4. Download dataset")
-        print("5. Train YOLO")
-        print("6. List directory")
-        print("7. Display confusion matrix")
-        print("8. Display results image")
+        print("2. Download weights")
+        print("3. Download dataset")
+        print("4. Train YOLO")
         print("0. Exit")
         print("\n==========================================\n")
-
 
         choice = input("Enter your choice: ")
 
@@ -260,7 +171,7 @@ def main():
                 else:
                     print_failure(f"Step {step} failed.")
             else:
-                print("\nInvalid choice. Please enter a number between 0 and 8.")
+                print("\nInvalid choice. Please enter a number between 0 and 4.")
         except ValueError:
             print("\nInvalid choice. Please enter a number.")
 
